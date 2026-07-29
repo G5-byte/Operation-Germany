544d87b93d204abff316a371a8c7ab8a41b690ab
G5-byte <nikhilchauhan1027@gmail.com>
2026-07-29 18:27:25 +0530

    Day 22 - Core Java basics

---
+++ Core_Java/DAY-22/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    public static void main(String[] args) {
+        String name = "Nikhil Chauhan";
+        int age = 23;
+        String college = "MIET";
+        String branch = "Computer Science and Engineering";
+        int semester = 7;
+        double cgpa = 7.8;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("College: " + college);
+        System.out.println("Branch: " + branch);
+        System.out.println("Semester: " + semester);
+        System.out.println("CGPA: " + cgpa);
+    }
+}
---
+++ Core_Java/DAY-22/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 23;
+        double cgpa = 7.8;
+        char grade = 'A';
+        boolean placed = false;
+        String country = "Germany";
+
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Placed: " + placed);
+        System.out.println("Country: " + country);
+    }
+    
+}
